86322769abd5708483fe565d058f1fb59057e9af
Amirhossein <129512946+itsamirdev@users.noreply.github.com>
2023-11-25 19:18:29 +0330

    Update views.py at Todos

--- Todos/views.py
+++ Todos/views.py
@@ -4,15 +4,14 @@ from .serializers import TodoSerializer
 from rest_framework.permissions import IsAuthenticated
 from .permissions import IsOwnerOrReadOnly
 
-"""
-The TodoViewSet class is a viewset that provides CRUD operations for the Todo model.
- It uses the TodoSerializer to serialize and deserialize data,
-and it has permission classes to ensure that only authenticated users can access the endpoints,
- and that only the owner of a todo can modify it
-"""
-
 
 class TodoViewSet(viewsets.ModelViewSet):
+    """
+    The TodoViewSet class is a viewset that provides CRUD operations for the Todo model.
+    It uses the TodoSerializer to serialize and deserialize data,
+    and it has permission classes to ensure that only authenticated users can access the endpoints,
+    and that only the owner of a todo can modify it
+    """
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
     search_fields = ['title', 'description']
